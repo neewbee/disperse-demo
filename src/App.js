cbc6ac308bdac14a71fece883b34358594994dea
stephen.liu <liuweiscu@gmail.com>
2023-10-10 10:20:40 +0800

    change a bit

--- src/App.js
+++ src/App.js
@@ -4,35 +4,31 @@ import { useGLTF, Outlines, Environment, Lightformer, useTexture, MeshTransmissi
 import { easing } from 'maath'
 
 export default function App() {
-  const jsx = <Canvas camera={{ position: [0, 0, 5], fov: 70 }}>
-    <color attach="background" args={['white']} />
-    <Model scale={1} rotation={[0, 0, 0]} position={[0, 0, 2]} />
-    <Floor />
-    <Environment preset="night">
-      <group rotation={[1, 0, 0]}>
-        <Lightformer intensity={1} position={[0, 10, -10]} scale={[10, 100, 1]} onUpdate={(self) => self.lookAt(0, 0, 0)} />
-        <Lightformer intensity={0.1} onUpdate={(self) => self.lookAt(0, 0, 0)} position={[-5, 1, -1]} rotation-y={Math.PI / 2} scale={[50, 10, 1]} />
-        <Lightformer intensity={100} onUpdate={(self) => self.lookAt(0, 0, 0)} position={[10, 1, 0]} rotation-y={-Math.PI / 2} scale={[50, 10, 1]} />
-        <Lightformer intensity={1} onUpdate={(self) => self.lookAt(0, 0, 0)} position={[0, 1, 0]} scale={[10, 100, 1]} />
-      </group>
-    </Environment>
-    {/* <Rig /> */}
-  </Canvas>;
   return (
-    jsx
+    <Canvas camera={{ position: [0, 0, 5], fov: 70 }}>
+      <color attach="background" args={['white']} />
+      <Model scale={0.4} rotation={[-Math.PI / 4, -0.5, 0.5]} position={[0, 0, 2]} />
+      <Floor />
+      <Environment preset="night">
+        <group rotation={[1, 0, 0]}>
+          <Lightformer intensity={1} position={[0, 10, -10]} scale={[10, 100, 1]} onUpdate={(self) => self.lookAt(0, 0, 0)} />
+          <Lightformer intensity={0.1} onUpdate={(self) => self.lookAt(0, 0, 0)} position={[-5, 1, -1]} rotation-y={Math.PI / 2} scale={[50, 10, 1]} />
+          <Lightformer intensity={100} onUpdate={(self) => self.lookAt(0, 0, 0)} position={[10, 1, 0]} rotation-y={-Math.PI / 2} scale={[50, 10, 1]} />
+          <Lightformer intensity={1} onUpdate={(self) => self.lookAt(0, 0, 0)} position={[0, 1, 0]} scale={[10, 100, 1]} />
+        </group>
+      </Environment>
+      {/* <Rig /> */}
+    </Canvas>
   )
 }
 
 function Model(props) {
   const ref = useRef()
-  const { nodes } = useGLTF('/untitled.glb')
-  useFrame((state, delta) => {
-    ref.current.geometry.center();
-    ref.current.rotation.x += delta / 3
-  })
+  const { nodes } = useGLTF('/shape.glb')
+  useFrame((state, delta) => (ref.current.rotation.x += delta / 3))
   return (
     <group {...props} dispose={null}>
-      <mesh ref={ref} geometry={nodes.Text.geometry} >
+      <mesh ref={ref} geometry={nodes.Cylinder.geometry}>
         <MeshTransmissionMaterial
           backside
           backsideThickness={-3}
